e0cd5419fdb98bae5ff18718420b1cb6ac2f1331
EVOKE <evoke.lektrique@gmail.com>
2021-11-06 20:56:30 +0330

    Add styles

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Home layout</title>
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     @yield('content')
